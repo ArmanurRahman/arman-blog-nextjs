fa65f49e0fea5ce524318715b459c96767b77120
Armanur Rahman <mubeenarman19@gmail.com>
2023-04-09 22:37:45 +0900

    add blog detail page

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,5 +1,31 @@
+import Header from "@/components/header/header";
+import Image from "next/image";
+import { Fragment } from "react";
+import classes from "../../styles/DetailPost.module.css";
+
 const PostDetailPage = () => {
-    return <div>Post detail page</div>;
+    return (
+        <Fragment>
+            <Header />
+            <section className={classes.section}>
+                <div className={classes.header}>
+                    <p className={classes.title}>Mastering Nextjs</p>
+                    <Image
+                        src='/image/blog/nextjs.png'
+                        alt='nextjs photo'
+                        width={200}
+                        height={120}
+                        className={classes.image}
+                    />
+                </div>
+                <div className={classes.devider}></div>
+                <p className={classes.details}>
+                    nextjs is fullstact framework. To understand nextjs need to
+                    know reactjs and javascript
+                </p>
+            </section>
+        </Fragment>
+    );
 };
 
 export default PostDetailPage;
